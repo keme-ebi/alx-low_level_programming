d303f7aac2447816ea8c02b3eac22ee3b13326f5
keme-ebi <kelvin6bolou@gmail.com>
2022-09-13 14:52:03 -0700

    a function that prints every minutes of the day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -13,6 +13,10 @@ void jack_bauer(void)
 	{
 		for (b = 0; b <= 9; b++)
 		{
+			if ((a == 2) && (b == 3) && (c == 5) && (d == 9))
+			{
+				break;
+			}
 			for (c = 0; c <= 5; c++)
 			{
 				for (d = 0; d <= 9; d++)
@@ -25,7 +29,7 @@ void jack_bauer(void)
 						_putchar((c % 10) + '0');
 						_putchar((d % 10) + '0');
 						_putchar('\n');
-					if ((a + b ==  2 + 3))
+					if ((a == 2) && (a + b + c + d == 19))
 					{
 						break;
 					}
