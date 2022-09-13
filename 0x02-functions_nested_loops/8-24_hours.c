aaf210d66454a3092a735a532c5eb006cbb0029e
keme-ebi <kelvin6bolou@gmail.com>
2022-09-13 10:35:01 -0700

    prints every minute of the day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -25,7 +25,7 @@ void jack_bauer(void)
 						_putchar((c % 10) + '0');
 						_putchar((d % 10) + '0');
 						_putchar('\n');
-					if ((a == 2) && (b == 3) && (c == 5) && (d == 9))
+					if ((a == 2) && (a + b + c + d = 19))
 					{
 						break;
 					}
