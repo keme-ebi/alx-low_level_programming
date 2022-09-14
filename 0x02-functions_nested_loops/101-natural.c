5c5f31d393757b6e172cecadb1e5ba138cc7a04c
keme-ebi <kelvin6bolou@gmail.com>
2022-09-14 08:46:24 -0700

    natural numbers below 10 that are multiples of 3 or 5

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - lists all natural numbers below 10
+ *	that are multiples of 3 or 5.
+ * Return: always 0.
+ */
+
+int main(void)
+{
+	int num, mult;
+
+	for (num = 1; num < 1024; num++)
+	{
+		if (((num % 3) == 0) || ((num % 5) == 0))
+			b = b + a;
+	}
+	printf("%d\n", b);
+	return(0);
+}
