ecbd317d6a8e0c09dfbeb9af57dcea401e5b770f
keme-ebi <kelvin6bolou@gmail.com>
2022-09-13 20:23:22 -0700

    function that prints every minute of the day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,29 +11,51 @@ void jack_bauer(void)
 
 	for (a = 0; a <= 2; a++)
 	{
-		for (b = 0; b <= 3; b++)
+		if (a < 2)
 		{
-			if (a < 2)
+			for (b = 0; b <= 9; b++)
 			{
-				for (b = 0; b <= 9; b++)
+				for (c = 0; c <= 5; c++)
 				{
-					for (c = 0; c <= 5; c++)
+					for (d = 0; d <= 9; d++)
 					{
-						for (d = 0; d <= 9; d++)
+						if (a <= 2)
 						{
-							if (a <= 2)
-							{
-								_putchar((a % 10) + '0');
-								_putchar((b % 10) + '0');
-								_putchar(':');
-								_putchar((c % 10) + '0');
-								_putchar((d % 10) + '0');
-								_putchar('\n');
-							if (a == 2 && b < 4 && c == 5 && d == 9)
-							{
-								break;
-							}
-							}
+							_putchar((a % 10) + '0');
+							_putchar((b % 10) + '0');
+							_putchar(':');
+							_putchar((c % 10) + '0');
+							_putchar((d % 10) + '0');
+							_putchar('\n');
+						if (a == 2 && b < 4 && c == 5 && d == 9)
+						{
+							break;
+						}
+						}
+					}
+				}
+			}
+		}
+		if (a == 2)
+		{
+			for (b = 0; b <= 3; b++)
+			{
+				for (c = 0; c <= 5; c++)
+				{
+					for(d = 0; d <= 9; d++)
+					{
+						if (a <= 2)
+						{
+							_putchar((a % 10) + '0');
+							_putchar((b % 10) + '0');
+							_putchar(',');
+							_putchar((c % 10) + '0');
+							_putchar((d % 10) + '0');
+							_putchar('\n');
+						if (a == 2 && b < 4 && c == 5 && d == 9)
+						{
+							break;
+						}
 						}
 					}
 				}
