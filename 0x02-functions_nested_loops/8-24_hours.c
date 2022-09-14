514fe4837e53b8e9c5f19fe715a67f45ce77c017
keme-ebi <kelvin6bolou@gmail.com>
2022-09-13 20:08:57 -0700

    function that prints every minute of the day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -16,23 +16,25 @@ void jack_bauer(void)
 			if (a < 2)
 			{
 				for (b = 0; b <= 9; b++)
-			}
-			for (c = 0; c <= 5; c++)
-			{
-				for (d = 0; d <= 9; d++)
 				{
-					if (a <= 2)
+					for (c = 0; c <= 5; c++)
 					{
-						_putchar((a % 10) + '0');
-						_putchar((b % 10) + '0');
-						_putchar(':');
-						_putchar((c % 10) + '0');
-						_putchar((d % 10) + '0');
-						_putchar('\n');
-					if (a == 2 && b < 4 && c == 5 && d == 9)
-					{
-						break;
-					}
+						for (d = 0; d <= 9; d++)
+						{
+							if (a <= 2)
+							{
+								_putchar((a % 10) + '0');
+								_putchar((b % 10) + '0');
+								_putchar(':');
+								_putchar((c % 10) + '0');
+								_putchar((d % 10) + '0');
+								_putchar('\n');
+							if (a == 2 && b < 4 && c == 5 && d == 9)
+							{
+								break;
+							}
+							}
+						}
 					}
 				}
 			}
