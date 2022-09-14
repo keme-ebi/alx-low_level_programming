7957bb99565d4e17141322cff7ecc8f3ff79fe2d
keme-ebi <kelvin6bolou@gmail.com>
2022-09-14 09:57:52 -0700

    Fibonacci sequence whose values do not exceed 4,000,000, write a program that finds and prints the sum of the even-valued terms

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - finds and prints the sum of the even-valued terms
+ * Return: always 0.
+ */
+
+int main(void)
+{
+	long int n1, n2, fs, f;
+
+	n1 = 1;
+	n2 = 2;
+	fs = f = 0;
+
+	for (fs = 0; fs <= 4000000; fs++)
+	{
+		fs = n1 + n2;
+		n1 = n2;
+		n2 = fs;
+		if((n1 % 2) == 0)
+			f = f + n1;
+	}
+	printf("%ld\n", f);
+	return (0);
+}
