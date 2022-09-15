111b7fe12aae84d5e72cb9a76a861254932bc4d5
keme-ebi <kelvin6bolou@gmail.com>
2022-09-15 13:06:02 -0700

    print triangle using #

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -19,8 +19,8 @@ void print_triangle(int size)
 		for (a = 0; a < size; a++)
 		{
 			for (b = 0; b < size; b++)
-				_putchar('#');
-			_putchar(' ');
+				_putchar(' ');
+			_putchar('#');
 		}
 		_putchar('\n');
 	}
