e4233a53532339f97171437f437499ebb4341ecb
keme-ebi <kelvin6bolou@gmail.com>
2022-09-15 06:39:51 -0700

    prints numbers except 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -12,9 +12,8 @@ void print_most_numbers(void)
 
 	for (num = '0'; num <= '9'; num++)
 	{
-		if (num == '2' && num == '4')
-			num++;
-		_putchar(num);
+		if (num != '2' && num != '4')
+			_putchar(num);
 	}
 	_putchar('\n');
 }
