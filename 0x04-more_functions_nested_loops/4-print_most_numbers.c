17a8d409cc7f71796db4b742003787464b0aa99e
keme-ebi <kelvin6bolou@gmail.com>
2022-09-15 06:29:13 -0700

    function that prints numbers except 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -12,8 +12,11 @@ void print_most numbers(void)
 
 	for (num = '0'; num <= '9'; num++)
 	{
-		if (num != '2' && num != '4')
-		_putchar(num);
+		if (num == '2' && num == '4')
+		{
+			num++;
+			_putchar(num);
+		}
 	}
 	_putchar('\n');
 }
