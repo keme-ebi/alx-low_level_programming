bad0e6409fa549d71773290da59a1940f05674f4
keme-ebi <kelvin6bolou@gmail.com>
2022-09-15 12:59:22 -0700

    print triangle using #

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_triangle - use # to print triangle
+ * @size: integer
+ * Return: void
+ */
+
+void print_triangle(int size)
+{
+	int a, b;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (a = 0; a < size; a++)
+		{
+			for (b = 0; b < size; b++)
+				_putchar('#');
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}
