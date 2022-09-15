0b22811331c6d403a9b870248a429c06fececdf6
keme-ebi <kelvin6bolou@gmail.com>
2022-09-15 07:31:00 -0700

    function that prints \ n times

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,18 +8,20 @@
 
 void print_diagonal(int n)
 {
-	int i;
+	int a, b;
 
-	for (i = 0; i < n; i++)
+	if (n <= 0)
 	{
-		if (n <= 0)
+		_putchar('\n');
+	}
+	else
+	{
+		for (a = 0; a < n; a++)
 		{
+			for (b = 0; b < a; b++)
+				_putchar(' ');
+			_putchar('\\');
 			_putchar('\n');
 		}
-		else
-		{
-			_putchar('\\');
-		}
 	}
-	_putchar('\n');
 }
