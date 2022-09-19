fa3c0e4cfd8a24c3c92e96ca95180e14c9a8f33d
keme-ebi <kelvin6bolou@gmail.com>
2022-09-19 12:52:15 -0700

    a function that prints half of a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: char
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int len, i;
+
+	for (len = 0; *(str + len) != '\0'; len++)
+	{
+		if (len % 2 == 1)
+			i = len / 2;
+		else if (len % 2 == 0)
+			i = (len - 1) / 2;
+		for (i++; i < len; i++)
+			_putchar(*(str + len));
+	}
+	_putchar('\n');
+}
