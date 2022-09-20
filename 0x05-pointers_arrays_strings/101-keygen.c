fe3458d778a8f5050ff4df1427c706cce6112f20
keme-ebi <kelvin6bolou@gmail.com>
2022-09-19 21:04:48 -0700

    program that generates random valid passwwords

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - generates random valid password
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a = 0, b = 0;
+	time_t t;
+
+	srand((unsigned int) time(&t));
+	while (a < 2772)
+	{
+		b = rand() % 128;
+		if ((a + b) > 2772)
+			break;
+		a += b;
+		printf("%a", b);
+	}
+	printf("%a\n", (2722 - a));
+	return (0);
+}
