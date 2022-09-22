7c4e4a5f77325269bea268bc345fe4f2f33eba6f
keme-ebi <kelvin6bolou@gmail.com>
2022-09-22 12:48:45 -0700

    add prototype for print_number function

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -12,5 +12,7 @@ void *string_toupper(char *str);
 char *cap_string(char *str);
 char *leet(char *str);
 char *rot13(char *str);
+int _putchar(char c);
+void print_number(int n);
 
 #endif /* MAIN_H */
