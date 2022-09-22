b0bb4843d0d35e2934572d08df97639826205297
keme-ebi <kelvin6bolou@gmail.com>
2022-09-22 12:06:51 -0700

    a function that capitalizes all words of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,17 +11,17 @@ char *cap_string(char *str)
 	int len = 0, i;
 	int special_char = {9, 10, 32, 33, 34, 40, 41, 44, 46, 59, 63, 123, 125};
 
-	if (str[len] >= 97 && str[len] <= 122)
-		str[len] = str[len] - 32;
+	if (*(str + len) >= 97 && *(str + len) <= 122)
+		*(str + len) = *(str + len) - 32;
 	len++;
-	while (str[len] != '\0')
+	while (*(str + len) != '\0')
 	{
 		for (i = 0; i < 13; i++)
 		{
-			if (str[len] == special_char[i])
+			if (*(str + len) == special_char[i])
 			{
-				if (((str[len] + 1) >= 97) && ((str[len] + 1) <= 122))
-					(str[len] + 1) = (str[len] + 1) - 32;
+				if ((*(str + (len + 1)) >= 97) && (*(str + (len + 1)) <= 122))
+					*(str + (len + 1)) = (str + (len + 1)) - 32;
 				break;
 			}
 		}
