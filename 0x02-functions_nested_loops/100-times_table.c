b47a2ab18129301f93a6a4d9c8bd7840011da522
keme-ebi <kelvin6bolou@gmail.com>
2022-09-22 14:11:25 -0700

    adjust for betty

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -16,30 +16,26 @@ void print_times_table(int n)
 			for (b = 0; b <= n; b++)
 			{
 				c = a * b;
+				_putchar(44);
+				_putchar(32);
 				if (b == 0)
 				{
 					_putchar('0');
 				}
 				else if (c < 10)
 				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
+					_putchar(32);
+					_putchar(32);
 					_putchar((c % 10) + '0');
 				}
 				else if (c >= 10 && c < 100)
 				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
+					_putchar(32);
 					_putchar(((c / 10) % 10) + '0');
 					_putchar((c % 10) + '0');
 				}
 				else if (c > 99 && c < 1000)
 				{
-					_putchar(',');
-					_putchar(' ');
 					_putchar((c / 100) + '0');
 					_putchar(((c / 10) % 10) + '0');
 					_putchar((c % 10) + '0');
