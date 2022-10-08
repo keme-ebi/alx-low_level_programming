2e39013c0f450057e80435930ccd05ff90565801
keme-ebi <kelvin6bolou@gmail.com>
2022-10-07 20:25:08 -0700

    program that multiplies two numbers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -3,42 +3,100 @@
 #include <stdlib.h>
 
 /**
- * digit - checks if a string contains a non-digit char
- * @s: string to be evaluated
- * Return: 0 if non-digit, otherwise 1
+ * _atoi_digit - convert a char to integer.
+ * @x: character to convert.
+ * Return: integer.
  */
-
-int digit(char *s)
+int _atoi_digit(char x)
 {
-	int i;
+	unsigned int res;
 
-	for (i = 0; s[i] < '0' || s[i] > '9'; i++)
-		return (0);
-	return (1);
+	if (x <= '9' && x >= '0')
+		res = x - '0';
+	return (res);
 }
 
 /**
- * slen - length of a string
- * @s: string
- * Return: string length
+ * _isNumber - Define if a string is a number.
+ * @argv: Pointer to string.
+ * Return: success (0).
  */
-
-int slen(char *s)
+int _isNumber(char *argv)
 {
 	int i;
 
-	for (i = 0; s[i] != '\0'; i++)
-		continue;
-	return (i);
+	for (i = 0; argv[i]; i++)
+		if (argv[i] < 48 || argv[i] > 57)
+			return (1);
+	return (0);
 }
+
 /**
- * error - error
+ * _calloc - allocate array of size * nmemb.
+ * @nmemb: number of elements
+ * @size: size of element.
+ * Return: pointer to array.
  */
-void error(void)
+void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	printf("Error\n");
-	exit(98);
+	char *tab;
+	unsigned int i;
+
+	tab = malloc(size * nmemb);
+	if (tab == NULL)
+		return (NULL);
+	for (i = 0; i < (size * nmemb); i++)
+		tab[i] = '0';
+	return (tab);
 }
+
+/**
+ * mul_array - multiply two arrays.
+ * @a1: first array
+ * @len1: length of array a1
+ * @a2:  char
+ * @a3: array for result
+ * @lena: length of array a3
+ * Return: pointer to array
+ */
+void *mul_array(char *a1, int len1, char a2, char *a3, int lena)
+{
+	int mul = 0, i, k;
+
+	k = lena;
+	for (i = len1 - 1; i >= 0 ; i--)
+	{
+		mul += (a1[i] - '0') * (a2 - '0') + (a3[k] - '0');
+		a3[k] = (mul % 10) + '0';
+		mul /= 10;
+		k--;
+	}
+	while (mul != 0)
+	{
+		mul += a3[k] - '0';
+		a3[k] = (mul % 10) + '0';
+		mul /= 10;
+		k--;
+	}
+	return (a3);
+}
+
+/**
+ * print_array - print all digits of array.
+ * @nb: number of elements to print.
+ * @a: array of elements.
+ */
+void print_array(char *a, int nb)
+{
+	int i = 0;
+
+	while (a[i] == '0' && (i + 1) < nb)
+		i++;
+	for (; i < nb; i++)
+		_putchar(a[i]);
+	_putchar('\n');
+}
+
 /**
  * main - multiplies two numbers
  * @argc: number of command line argument
@@ -46,46 +104,36 @@ void error(void)
  * Return: 0
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char *arv[])
 {
-	unsigned int len, len1, len2, i, carry, digit1, digit2, *result, a = 0;
-	char *s1, *s2;
+	int i, c, len1, len2, lenres;
+	char E[6] = {'E', 'r', 'r', 'o', 'r', '\n'};
+	char *tabres;
 
-	s1 = argv[1], s2 = argv[2];
-	if (argc != 3 || !digit(s1) || !digit(s2))
-		error();
-	len1 = slen(s1);
-	len2 = slen(s2);
-	len = len1 + len2 + 1;
-	result = malloc(sizeof(unsigned int) * len);
-	if (!result)
-		return (1);
-	for (i = 0; i <= len1 + len2; i++)
-		result[i] = 0;
-	for (len1 = len1 - 1; len1 >= 0; len1--)
+	if (argc != 3 || _isNumber(argv[1]) == 1 || _isNumber(argv[2]) == 1)
 	{
-		digit1 = s1[len1] - '0';
-		carry = 0;
-		for (len2 = len2 - 1; len2 >= 0; len2--)
-		{
-			digit2 = s2[len2] - '0';
-			carry += result[len1 + len2 + 1] + (digit1 * digit2);
-			result[len1 + len2 + 1] = carry % 10;
-			carry /= 10;
-		}
-		if (carry > 0)
-			result[len1 + len2 + 1] += carry;
+		for (i = 0; i < 6; i++)
+			_putchar(E[i]);
+		exit(98);
 	}
-	for (i = 0; i < len - 1; i++)
+	for (len1 = 0; argv[1][len1]; len1++)
+		;
+	for (len2 = 0; argv[2][len2]; len2++)
+		;
+	lenres = len1 + len2;
+	tabres = _calloc(lenres, sizeof(int));
+	if (tabres == NULL)
 	{
-		if (result[i])
-			a = 1;
-		if (a)
-			_putchar(result[i] + '0');
+		free(tabres);
+		return (0);
 	}
-	if (!a)
-		_putchar('0');
-	_putchar('\n');
-	free(result);
+	for (i = len2 - 1, c = 0; i >= 0; i--)
+	{
+		tabres = mul_array(argv[1], len1, argv[2][i], tabres, (lenres - 1 - c));
+		c++;
+	}
+	print_array(tabres, lenres);
+	free(tabres);
+	exit(EXIT_SUCCESS);
 	return (0);
 }
