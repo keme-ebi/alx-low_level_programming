ae384a6d396551f9bdd34cab0a707d816461dbca
keme-ebi <kelvin6bolou@gmail.com>
2022-10-12 18:42:29 -0700

    functions to carry out simple calculation operations

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,67 @@
+#include "calc.h"
+#include <stdio.h>
+
+/**
+ * op_add - adds two numbers
+ * @a: first number
+ * @b: second number
+ * Return: addition of both numbers
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtracts two numbers
+ * @a: first number
+ * @b: second number
+ * Return: subtraction
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiply two numbers
+ * @a: first number
+ * @b: second number
+ * Return: multiplication
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divides two numbers
+ * @a: first number
+ * @b: second number
+ * Return: division
+ */
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a / b);
+}
+
+/**
+ * op_mod - returns the remainder
+ * @a: first number
+ * @b: second number
+ * Return: remainder
+ */
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
+}
