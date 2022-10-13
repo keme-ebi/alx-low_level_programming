c87796cdb9bfe81d9b2a1ead867e69aee985a7a4
keme-ebi <kelvin6bolou@gmail.com>
2022-10-13 07:17:34 -0700

    code check

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,5 +1,6 @@
 #include "3-calc.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - checks the code
@@ -9,10 +10,27 @@
  */
 int main(int argc, char *argv[])
 {
+	int a1, a2, rs;
+	int (*op)(int, int);
+
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
+
+	a1 = atoi(argv[1]);
+	a2 = atoi(argv[3]);
+	op = get_op_func(argv[2]);
+
+	if (!op)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	rs = func(a1, a2);
+
+	printf("%d\n", rs);
 	return (0);
 }
