e2744dabf3d78a107f942c572101bd41aad191de
keme-ebi <kelvin6bolou@gmail.com>
2022-10-12 19:08:35 -0700

    main

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,12 @@
+#include "3-calc.h"
+#include <stdio.h>
+
+/**
+ * main - checks the code
+ * @argc: number of command line arguments
+ * @argv: arguments passed
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+}
