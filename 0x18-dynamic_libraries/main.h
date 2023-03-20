ea8e5d6479f7dea0d249afdfd59051426c0880c8
keme-ebi <kelvin6bolou@gmail.com>
2023-03-20 14:35:40 +0000

    dynamic library files

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,5 +1,8 @@
 #ifndef MAIN_H
-#define
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
 
 int _putchar(char c);
 int _islower(int c);
@@ -11,7 +14,7 @@ int _strlen(char *s);
 void _puts(char *s);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
-char *_strcat(char *dest char *src);
+char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
