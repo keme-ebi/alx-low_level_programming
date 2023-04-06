2f2234f485fc9574e526bab398db534b55f0051e
keme-ebi <kelvin6bolou@gmail.com>
2023-04-06 16:32:59 -0700

    fix

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -14,7 +14,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *temp = *h;
 	unsigned int nth = 0;
 
-	if (idx == 0 || temp->next == NULL)
+	if (idx == 0 || temp->next == NULL || *h == NULL)
 		new = add_dnodeint(h, n);
 
 	if (temp == NULL && idx != 0)
