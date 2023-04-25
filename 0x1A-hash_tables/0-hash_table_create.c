34d8f0da326ee6a738f520b3d65dc738125d8a32
keme-ebi <kelvin6bolou@gmail.com>
2023-04-25 17:44:35 +0000

    add condition if something went wrong

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,6 +11,9 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *new_table = malloc(sizeof(hash_table_t));
 	unsigned int i;
 
+	if (!new_table)
+		return (NULL);
+
 	new_table->size = size;
 	new_table->array = calloc(new_table->size, sizeof(hash_node_t));
 
